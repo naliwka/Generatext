e523efef6bda60905de843aece999f58c569b643
naliwka <avierina.nataliia@gmail.com>
2023-04-13 21:19:08 +0300

    Update Program.cs

--- Generatext/Program.cs
+++ Generatext/Program.cs
@@ -10,9 +10,30 @@ namespace Generatext
     {
         static void Main(string[] args)
         {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "Books");
+            string[] files = Directory.GetFiles(folderPath);
             var text = File.ReadAllText("HarryPotterText.txt");
             var sentences = SentencesParser.ParseSentences(text);
             var frequency = FrequencyAnalysis.GetMostFrequentNextWords(sentences);
+
+            Console.WriteLine("Enter the book (choose the number).");
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {Path.GetFileName(files[i])}");
+            }
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            // switch or smth to bring SentencesParcer.cs choisen path
+            while (true)
+            {
+                Console.Write("Enter the first word (for example, he): ");
+                var beginning = Console.ReadLine();
+                Console.Write("Enter the count of words in sentence: ");
+                int countWords = Convert.ToInt32(Console.ReadLine());
+                if (string.IsNullOrEmpty(beginning)) return;
+                var phrase = TextGenerator.ContinuePhrase(frequency, beginning.ToLower(), countWords);
+                Console.WriteLine(phrase);
+            }
         }
+
     }
 }
