ea69f82583c0fb61c394d2faaaf32c8817223e4f
naliwka <avierina.nataliia@gmail.com>
2023-04-13 19:02:56 +0300

    Update TextGenerator.cs

--- Generatext/TextGenerator.cs
+++ Generatext/TextGenerator.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generatext
 {
-    internal class TextGenerator
+    static class TextGenerator
     {
+        private static string GetNextWord(string total,
+            Dictionary<string, string> nextWords)
+        {
+            var start = total.Split(' ').Skip(total.Split(' ').Count() - 2);
+            var startBigram = start.LastOrDefault();
+            var startTrigram = start.Count() > 1 ? start.FirstOrDefault() + " " + startBigram : "";
+            return nextWords.ContainsKey(startTrigram) ? total + " " + nextWords[startTrigram] :
+                nextWords.ContainsKey(startBigram) ? total + " " + nextWords[startBigram] : total;
+        }
+
+        public static string ContinuePhrase(Dictionary<string, string> nextWords,
+            string phraseBeginning, int wordsCount) =>
+            Enumerable.Repeat("", wordsCount)
+                .Aggregate(string
+                .Join(" ", phraseBeginning
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)),
+                (total, next) => GetNextWord(total, nextWords));
     }
 }
