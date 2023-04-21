d20a48258e2feae512c15d4a1d72e9741a3e2055
naliwka <avierina.nataliia@gmail.com>
2023-04-22 00:40:44 +0300

    Move methods from Program.cs to a new class

---
+++ Generatext/BookSelector.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+
+namespace Generatext
+{
+    static class BookSelector
+    {
+        public static void PrintListOfBooks(string[] files)
+        {
+            Console.WriteLine("List of books:");
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine($"\t{i + 1}. {Path.GetFileName(files[i])}");
+            }
+            Console.WriteLine("\t0. {Your book}");
+        }
+        public static string BookChoice(string[] files)
+        {
+            string input;
+            int userChoice;
+            string text = "";
+            int attempts = 0;
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.Write("\nChoose the book (enter the number): ");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out userChoice) && userChoice <= files.Length)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (i + 1 == userChoice)
+                        {
+                            text = File.ReadAllText(files[i]);
+                        }
+                        else if (userChoice == 0)
+                        {
+                            string userFilePath;
+                            do
+                            {
+                                Console.WriteLine("\n" + @"Enter the path to your book (C:\\example\\file.txt):");
+                                userFilePath = Console.ReadLine();
+                                if (IsValidFilePath(userFilePath))
+                                {
+                                    text = File.ReadAllText(userFilePath);
+                                }
+                                else
+                                {
+                                    InvalidInput(attempts);
+                                    attempts++;
+                                }
+                            }
+                            while (!IsValidFilePath(userFilePath));
+                        }
+                    }
+                    isValid = true;
+                }
+                else
+                {
+                    InvalidInput(attempts);
+                    attempts++;
+                }
+            }
+            return text;
+        }
+        public static void InvalidInput(int attempts)
+        {
+            if (attempts < 2)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Invalid input. Try again!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Too many attempts.");
+                Console.ResetColor();
+                RetryOrExit();
+            }
+        }
+        public static bool IsValidFilePath(string path)
+        {
+            return File.Exists(path);
+        }
+        public static void RetryOrExit()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\nPress any key to try again or '0' to exit");
+            Console.ResetColor();
+            string choice = Console.ReadLine();
+            if (choice == "0")
+            {
+                Environment.Exit(0);
+            }
+        }
+        public static string GetUserBeginningWord()
+        {
+            var beginning = "";
+            int attempts = 0;
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.Write("\nEnter the first word (for example, start): ");
+                beginning = Console.ReadLine();
+                if (SentencesParser.IsWord(beginning))
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    InvalidInput(attempts);
+                    attempts++;
+                }
+            }
+            return beginning;
+        }
+        public static int GetUserWordsCount()
+        {
+            int countWords = 0;
+            int attempts = 0;
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.Write("\nEnter the count of words in sentence: ");
+                string inputCount = Console.ReadLine();
+                if (int.TryParse(inputCount, out countWords))
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    InvalidInput(attempts++);
+                }
+            }
+            return countWords;
+        }
+    }
+}
--- Generatext/Program.cs
+++ Generatext/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generatext
 {
@@ -12,144 +9,19 @@ namespace Generatext
         {
             string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "Books");
             string[] files = Directory.GetFiles(folderPath);
-            PrintListOfBooks(files);
-            var sentences = SentencesParser.ParseSentences(BookChoice(files));
+            BookSelector.PrintListOfBooks(files);
+            var sentences = SentencesParser.ParseSentences(BookSelector.BookChoice(files));
             var frequency = FrequencyAnalysis.GetMostFrequentNextWords(sentences);
 
             while (true)
             {
-                string beginning = GetUserBeginningWord();
-                int countWords = GetUserWordsCount();
+                string beginning = BookSelector.GetUserBeginningWord();
+                int countWords = BookSelector.GetUserWordsCount();
                 if (string.IsNullOrEmpty(beginning)) return;
                 var phrase = TextGenerator.ContinuePhrase(frequency, beginning.ToLower(), countWords);
-                Console.WriteLine($"Your sentence: \n{phrase}.");
-                RetryOrExit();
+                Console.WriteLine($"\nYour sentence: \n{phrase}.");
+                BookSelector.RetryOrExit();
             }
-        }
-
-        public static void PrintListOfBooks(string[] files)
-        {
-            Console.WriteLine("List of books:");
-            for (int i = 0; i < files.Length; i++)
-            {
-                Console.WriteLine($"{i + 1}. {Path.GetFileName(files[i])}");
-            }
-            Console.WriteLine("0. {Your book}");
-        }
-        public static string BookChoice(string[] files)
-        {
-            string input;
-            int userChoice;
-            string text = "";
-            int attempts = 0;
-            bool isValid = false;
-            while (!isValid)
-            {
-                Console.WriteLine("\nChoose the book (enter the number):");
-                input = Console.ReadLine();
-                if (int.TryParse(input, out userChoice) && userChoice <= files.Length)
-                {
-                    for (int i = 0; i < files.Length; i++)
-                    {
-                        if (i + 1 == userChoice)
-                        {
-                            text = File.ReadAllText(files[i]);
-                        }
-                        else if (userChoice == 0)
-                        {
-                            string userFilePath;
-                            do
-                            {
-                                Console.WriteLine(@"Enter the path to your book (C:\\example\\file.txt):");
-                                userFilePath = Console.ReadLine();
-                                if (IsValidFilePath(userFilePath))
-                                {
-                                    text = File.ReadAllText(userFilePath);
-                                }
-                                else
-                                {
-                                    InvalidInput(attempts);
-                                    attempts++;
-                                }
-                            }
-                            while (!IsValidFilePath(userFilePath));
-                        }
-                    }
-                    isValid = true;
-                }
-                else
-                {
-                    InvalidInput(attempts);
-                    attempts++;
-                }
-            }
-            return text;
-        }
-        public static void InvalidInput(int attempts)
-        {
-            if (attempts < 2)
-            {
-                Console.WriteLine("Invalid input. Try again!");
-            }
-            else
-            {
-                Console.WriteLine("Too many attempts.");
-                RetryOrExit();
-            }
-        }
-        public static bool IsValidFilePath(string path)
-        {
-            return File.Exists(path);
-        }
-        public static void RetryOrExit()
-        {
-            Console.WriteLine("Press any key to try again or '0' to exit");
-            string choice = Console.ReadLine();
-            if (choice == "0")
-            {
-                Environment.Exit(0);
-            }
-        }
-        public static string GetUserBeginningWord()
-        {
-            var beginning = "";
-            int attempts = 0;
-            bool isValid = false;
-            while (!isValid)
-            {
-                Console.WriteLine("Enter the first word (for example, man): ");
-                beginning = Console.ReadLine();
-                if (SentencesParser.IsWord(beginning))
-                {
-                    isValid = true;
-                }
-                else
-                {
-                    InvalidInput(attempts);
-                    attempts++;
-                }
-            }
-            return beginning;
-        }
-        public static int GetUserWordsCount()
-        {
-            int countWords = 0;
-            int attempts = 0;
-            bool isValid = false;
-            while (!isValid)
-            {
-                Console.WriteLine("Enter the count of words in sentence: ");
-                string inputCount = Console.ReadLine();
-                if (int.TryParse(inputCount, out countWords))
-                {
-                    isValid = true;
-                }
-                else
-                {
-                    InvalidInput(attempts++);
-                }
-            }
-            return countWords;
         }
     }
 }
